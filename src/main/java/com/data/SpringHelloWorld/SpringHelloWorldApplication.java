12d225067b16458a479ff6bf693109fe70b356e5
LENOVO <LENOVO@omkarsPC>
2024-05-31 12:40:50 +0530

    Hello World

---
+++ src/main/java/com/data/SpringHelloWorld/SpringHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.data.SpringHelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/data/SpringHelloWorld/SpringHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.SpringHelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
